dad1c1ca64361e9a3f39df67ee5b225b49cd2ac8
lostmygithubaccount <cody.dkdc2@gmail.com>
2020-10-31 20:51:56 -0700

    saving

--- code/magic.py
+++ code/magic.py
@@ -1,4 +1,31 @@
 import time 
 
+from mpi4py import MPI 
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+print("\n\n")
+print("rank: ")
+print(rank)
+print("\n\n")
+
+from dask_mpi import initialize
+initialize()
+
+from dask.distributed import Client 
+
+counter = 0
 while True:
+
+    print(counter)
+    counter += 1
     time.sleep(10)
+
+    if rank == 1:
+        print("rank1")
+        try:
+            client = Client()
+            print(client)
+        except:
+            pass
